abed7c7f137f70820cab0cb62d5fb54612b61e87
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-05-22 19:34:54 +0200

    3.4 CreatePostTest: dla zatwierdzonego i nowego uzytkownika

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class CreatePostTest extends FunctionalTests {
+
+	@Test
+    public void createPostShouldRequireCONFIRMEDAccountStatus() {
+		JSONObject jsonObj = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/1/post");
+		
+        JSONObject jsonObj2 = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj2.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/2/post");
+    }
+}
