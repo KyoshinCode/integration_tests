c46d003f2012e7e145d9ba946cf1e2fcb70be4aa
Konrad Gos <goskonrad@outlook.com>
2017-05-22 17:50:01 +0200

    [3.1] + creationLikePostEntity test

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -1,9 +1,18 @@
 package edu.iis.mto.blog.domain.repository;
 
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.model.User;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * Created by Konrad Gos on 22.05.2017.
@@ -20,4 +29,27 @@ public class LikePostRepositoryTest {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
+    @Before
+    public void setUp() {
+        List<User> users = userRepository.findAll();
+        BlogPost blogPost = new BlogPost();
+        blogPost.setEntry("entry text");
+        blogPost.setId(new Long((1)));
+        blogPost.setUser(users.get(0));
+        blogPostRepository.save(blogPost);
+
+        List<BlogPost> blogPosts = blogPostRepository.findAll();
+
+        LikePost likePost = new LikePost();
+        likePost.setId(new Long(1));
+        likePost.setPost(blogPosts.get(0));
+        likePost.setUser(users.get(0));
+        repository.save(likePost);
+    }
+
+    @Test
+    public void creationLikePostEntity() {
+        List<LikePost> likePosts = repository.findAll();
+        Assert.assertThat(likePosts, Matchers.hasSize(1));
+    }
 }
