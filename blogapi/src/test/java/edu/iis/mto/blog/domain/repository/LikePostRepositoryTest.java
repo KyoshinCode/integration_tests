417ea577e6c2d0c7fc36997862a115e764e7e164
Piotr Dudek <194936@edu.p.lodz.pl>
2017-05-22 22:58:25 +0200

    [3.1] REFACTORING CODE

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -1,6 +1,5 @@
 package edu.iis.mto.blog.domain.repository;
 
-import edu.iis.mto.blog.domain.BlogManager;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
@@ -15,16 +14,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by Piotrek on 22.05.2017.
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class LikePostRepositoryTest {
-    private BlogManager blogManager;
-
     @Autowired
     private LikePostRepository repository;
 
@@ -33,10 +28,6 @@ public class LikePostRepositoryTest {
 
     @Autowired
     private BlogPostRepository blogPostRepository;
-
-    private LikePost likePost;
-    private User user;
-    private BlogPost blogPost;
 
     @Test
     public void postEntityCorrectCreate(){
@@ -58,7 +49,7 @@ public class LikePostRepositoryTest {
     public void findByUserAndPostCorrectWorking(){
         List<BlogPost> foundBlogPost = blogPostRepository.findAll();
         List<User> foundUser = userRepository.findAll();
-        List<LikePost> foundLikePost = repository.findAll();
+
         Optional<LikePost> likePost = repository.findByUserAndPost(foundUser.get(0), foundBlogPost.get(0));
         Assert.assertThat(likePost.get().getUser(), Matchers.equalTo(foundUser.get(0)));
     }
