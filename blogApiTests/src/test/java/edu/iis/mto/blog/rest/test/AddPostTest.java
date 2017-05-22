9d36d22d66e26b34d1ebb720651023872923f4eb
Konrad Gos <goskonrad@outlook.com>
2017-05-22 22:00:33 +0200

    [3.4] * fixed confirmedUserCanAddPost test - now it works only for confirmed users

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
@@ -14,8 +14,13 @@ public class AddPostTest extends FunctionalTests{
     @Test
     public void confirmedUserCanAddPost() {
         JSONObject jsonObj = new JSONObject().put("entry", "entry text");
+        
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
                 .post("/blog/user/1/post");
+
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/2/post");
     }
 }
