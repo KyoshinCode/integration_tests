d1a2adcbddfa6c74177c94da7a26fff0c10593f0
patrykxk <patrix215@gmail.com>
2017-05-22 19:18:05 +0200

    3.1 dodano test sprawdzajacy dodanie jednej encji do repozytorium

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -2,6 +2,8 @@ package edu.iis.mto.blog.domain.repository;
 
 import java.util.List;
 
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,7 +14,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
 
@@ -25,17 +26,38 @@ public class LikePostRepositoryTest {
     @Autowired
     private LikePostRepository repository;
     private LikePost likePost;
-
+    private User user;
+    private BlogPost blogPost;
     @Before
     public void setUp() {
         likePost = new LikePost();
-        likePost.setPost(null);
-        likePost.setUser(null);
+        blogPost = new BlogPost();
+        user = new User();
+
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+        user.setAccountStatus(AccountStatus.NEW);
+        blogPost.setUser(user);
+        blogPost.setEntry("Testowy wpis");
+        entityManager.persist(user);
+        entityManager.persist(blogPost);
+
+        likePost.setPost(blogPost);
+        likePost.setUser(user);
     }
 
     @Test
     public void shouldFindNoLikePostsIfRepositoryIsEmpty() {
         List<LikePost> likePosts = repository.findAll();
         Assert.assertThat(likePosts, Matchers.hasSize(0));
+    }
+    @Test
+    public void shouldFindOneLikePostIfRepositoryContainsOneLikePostEntity() {
+        LikePost persistedLikePost = entityManager.persist(likePost);
+        List<LikePost> likePosts = repository.findAll();
+
+        Assert.assertThat(likePosts, Matchers.hasSize(1));
+        Assert.assertThat(likePosts.get(0).getUser(), Matchers.equalTo(persistedLikePost.getUser()));
     }
 }
