3774a209e633a4ba5b1a5d729a839ef8472e435d
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-05-22 20:42:57 +0200

    3.4 AddLikeToPost: dla statusu po ponownym polubieniu

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikeToPost.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikeToPost.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.blog.rest.test;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -23,6 +25,29 @@ public class AddLikeToPost extends FunctionalTests {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
                 .post("/blog/user/2/like/1");
+	}
+	
+	@Test
+    public void addLikeTwiceShouldNotChangePostStatus() {
+		JSONObject jsonObj = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/1/post");
+        
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .post("/blog/user/3/like/1");
+        
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+	        .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+	        .post("/blog/user/3/like/1");
+        
+        int likesCount = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+	        .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+	        .get("/blog/user/1/post").then().extract().
+	        jsonPath().getInt("likesCount[0]");
+        
+        Assert.assertThat(likesCount, Matchers.equalTo(1));
 	}
 
 }
