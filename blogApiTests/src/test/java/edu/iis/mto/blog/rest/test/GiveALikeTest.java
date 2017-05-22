35075fbae07e3e9329a1a97655e5e23cb73cdc56
Konrad Gos <goskonrad@outlook.com>
2017-05-22 21:34:21 +0200

    [3.4] + confirmedUserIsNotAbleToLikeHisOwnPost test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GiveALikeTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GiveALikeTest.java
@@ -21,4 +21,12 @@ public class GiveALikeTest extends FunctionalTests  {
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
                 .post("/blog/user/2/like/1");
     }
+
+    @Test
+    public void confirmedUserIsNotAbleToLikeHisOwnPost() {
+        JSONObject jsonObj = new JSONObject();
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/1/like/1");
+    }
 }
