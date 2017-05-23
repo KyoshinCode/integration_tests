3c1844bab89654d2ba81618bf722adf4ba134aa2
patrykxk <patrix215@gmail.com>
2017-05-23 14:23:04 +0200

    3.4.4 test wyszukiwania postow uzytkownika

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindingUserPostsTest.java
@@ -0,0 +1,46 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Patryk on 2017-05-23.
+ */
+
+public class FindingUserPostsTest {
+    @Test
+    public void findingPostUserWithStatusRemovedShouldReturnError() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .get("/blog/user/4/post");
+    }
+
+    @Test
+    public void findingPostsShouldReturnProperLikeCount() {
+        JSONObject jsonObj = new JSONObject().put("entry", "post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/1/post");
+
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .post("/blog/user/3/like/1");
+
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .post("/blog/user/5/like/1");
+
+        int likesCount = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .get("/blog/user/1/post").then().extract().
+                        jsonPath().getInt("likesCount[0]");
+
+        assertThat(likesCount, Matchers.equalTo(2));
+    }
+}
