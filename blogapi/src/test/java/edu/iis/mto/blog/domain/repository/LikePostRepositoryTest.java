3f7c0bc0394b029594e3a9bf627f9ddbea391d98
Piotr Dudek <194936@edu.p.lodz.pl>
2017-05-23 15:09:08 +0200

    [3.1] BUG FIX vol.2

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -5,6 +5,7 @@ import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,24 @@ public class LikePostRepositoryTest {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
+    @Before
+    public void setUp(){
+        List<User> users = userRepository.findAll();
+        BlogPost blogPost = new BlogPost();
+        blogPost.setEntry("test");
+        blogPost.setId((long) 30);
+        blogPost.setUser(users.get(0));
+        blogPostRepository.save(blogPost);
+
+        List<BlogPost> blogPosts = blogPostRepository.findAll();
+
+        LikePost likePost = new LikePost();
+        likePost.setId(null);
+        likePost.setPost(blogPosts.get(0));
+        likePost.setUser(users.get(0));
+        repository.save(likePost);
+    }
+
     @Test
     public void postEntityCorrectCreate(){
         List<LikePost> foundLikePosts = repository.findAll();
@@ -40,6 +59,8 @@ public class LikePostRepositoryTest {
         List<LikePost> foundLikePosts = repository.findAll();
         String foundEntry = foundLikePosts.get(0).getPost().getEntry();
         foundLikePosts.get(0).getPost().setEntry("testing");
+        repository.save(foundLikePosts);
+        foundLikePosts = repository.findAll();
         String newEntry = foundLikePosts.get(0).getPost().getEntry();
         Assert.assertThat(foundEntry, Matchers.equalTo("test"));
         Assert.assertThat(newEntry, Matchers.equalTo("testing"));
