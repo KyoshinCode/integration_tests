b8ad93607e44fe343eb7e50042c47ed609a6d7b6
patrykxk <patrix215@gmail.com>
2017-05-23 13:45:42 +0200

    3.4.3 test autor nie moze dac like do swojego postu

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
@@ -17,4 +17,13 @@ public class AddLikePostTest {
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
                 .post("/blog/user/2/like/1");
     }
+
+    @Test
+    public void authorShouldNotLikeOwnPost() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/1/like/1");
+    }
+
 }
