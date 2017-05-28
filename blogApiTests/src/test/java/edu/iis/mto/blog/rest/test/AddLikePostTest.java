7937760ea3b96152c448a8afc3e48fe530bde458
Robert <ropak@wp.pl>
2017-05-28 12:58:56 +0200

    + Dodany test sprawdzający czy zatwierdzony użytkownik może dodać LikePost

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
@@ -0,0 +1,17 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class AddLikePostTest extends FunctionalTests {
+
+    @Test
+    public void confirmedUserCanAddLike() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK)
+                .when().post("/blog/user/3/like/1");
+    }
+}
