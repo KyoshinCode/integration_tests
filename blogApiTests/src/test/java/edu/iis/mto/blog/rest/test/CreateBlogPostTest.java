04f5345ab4539549fbf01f4d206d9a65d80048cb
Robert <ropak@wp.pl>
2017-05-28 12:40:48 +0200

    + Dodany test sprawdzający czy nowy użytkownik nie może dodać BlogPost. !!! Uwaga, test nie przechodzi !!!

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateBlogPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateBlogPostTest.java
@@ -16,4 +16,12 @@ public class CreateBlogPostTest extends FunctionalTests {
                 .body(request.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED)
                 .when().post("/blog/user/1/post");
     }
+    @Test
+    public void newUserCanNotAddPost() {
+        JSONObject request = new JSONObject()
+                .put("entry", "Invalid creation");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(request.toString()).expect().log().all().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .when().post("/blog/user/2/post");
+    }
 }
