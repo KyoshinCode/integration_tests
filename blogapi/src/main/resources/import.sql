619bac515d14b305cb16c492f8585e79d1d21b01
Robert <ropak@wp.pl>
2017-05-28 12:58:27 +0200

    + Rozszerzenie domyślnych danych testowych

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
+insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'pako@domain.com', 'Robert', 'Pakowski')
+insert into blog_post (id, entry, user_id) values (null, 'Default post', 1)
