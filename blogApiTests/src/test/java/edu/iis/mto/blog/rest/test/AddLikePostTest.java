2eb1fe98282221da13949fb0c636f9521309bcd9
Robert <ropak@wp.pl>
2017-05-28 13:35:00 +0200

    + Dodany test sprawdzający czy ponowne likowanie zwróci false

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
@@ -3,8 +3,11 @@ package edu.iis.mto.blog.rest.test;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 
 public class AddLikePostTest extends FunctionalTests {
 
@@ -27,5 +30,13 @@ public class AddLikePostTest extends FunctionalTests {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST)
                 .when().post("/blog/user/1/like/1");
+    }
+
+    @Test
+    public void repeatedLikingShouldNotChangeAnything() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK)
+                .body(is("false"))
+                .when().post("/blog/user/5/like/1");
     }
 }
