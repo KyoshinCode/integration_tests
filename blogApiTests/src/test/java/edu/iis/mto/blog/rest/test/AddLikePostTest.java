3dd3c085874d786cd7dc5a92a8893d9cffa0f75a
Robert <ropak@wp.pl>
2017-05-28 13:01:09 +0200

    + Dodany test sprawdzający czy nie można polubić własnego posta

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
@@ -14,10 +14,18 @@ public class AddLikePostTest extends FunctionalTests {
                 .expect().log().all().statusCode(HttpStatus.SC_OK)
                 .when().post("/blog/user/3/like/1");
     }
+
     @Test
     public void newUserCanNotAddLike() {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST)
                 .when().post("/blog/user/2/like/1");
+    }
+
+    @Test
+    public void cannotLikeOwnPost() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when().post("/blog/user/1/like/1");
     }
 }
