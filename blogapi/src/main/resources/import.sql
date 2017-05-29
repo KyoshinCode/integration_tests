84576b4c523f29f7b8f151c894eefd421b4587a8
Sasho <Sasho>
2017-05-29 22:17:01 +0200

    3.4 4 Dodano testy sprawdzajace wyszukiwanie postow uzytkownika

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -6,3 +6,5 @@ insert into user (id, account_status, email, first_name, last_name) values (null
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'beee@domain.com', 'Welniana', 'Owieczka')
 insert into blog_post (id, entry, user_id) values (null, 'Nowy post', 1)
 insert into like_post (id, post_id, user_id) values (null, 1, 5)
+insert into blog_post (id, entry, user_id) values (null, 'Post usunietego uzytkownika', 3)
+insert into blog_post (id, entry, user_id) values (null, 'Lubiany post', 1)
