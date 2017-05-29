c32aed204bf9c9817c649ddb12b0d4cbc83763e7
ciupa.kamil <194928@edu.p.lodz.pl>
2017-05-29 23:19:42 +0200

    3.1 Test braku LikePosts w repozytorium

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -1,22 +1,41 @@
 package edu.iis.mto.blog.domain.repository;
 
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class LikePostRepositoryTest {
 
     @Autowired
     private TestEntityManager entityManager;
 
-    
     @Autowired
     private LikePostRepository repository;
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Autowired
+    private BlogPostRepository blogRepository;
 	
 	User user;
+	BlogPost post;
+	LikePost likePost;
 	
 	@Before
 	public void setup() {
@@ -25,6 +44,23 @@ public class LikePostRepositoryTest {
 	        user.setLastName("Nowak");
 	        user.setEmail("john@domain.com");
 	        user.setAccountStatus(AccountStatus.NEW);
+	        
+	        post = new BlogPost();
+	        post.setEntry("new");
+	        post.setUser(user);
+	        
+	        likePost = new LikePost();
+	        likePost.setUser(user);
+	        likePost.setPost(post);
+	}
+	
+	@Test
+	public void shouldNotFindLikePostsIfRepositoryIsEmpty() {
+		
+		userRepository.save(user);
+		List<LikePost> likedPosts = repository.findAll();
+		
+		Assert.assertThat(likedPosts, Matchers.hasSize(0));
 	}
 	
 }
