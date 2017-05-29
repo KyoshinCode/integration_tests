d425d4db7e8f03d3dc040b0dfc8e9867b6d068d2
Sasho <Sasho>
2017-05-29 22:30:13 +0200

    Fix 3.4.4 - correctLikeNumber Test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostTest.java
@@ -22,12 +22,6 @@ public class SearchPostTest extends FunctionalTests {
 
     @Test
     public void correctLikeNumber() throws Exception {
-        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .expect().log().all().statusCode(HttpStatus.SC_OK).when()
-                .post("/blog/user/4/like/3");
-        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .expect().log().all().statusCode(HttpStatus.SC_OK).when()
-                .post("/blog/user/5/like/3");
         int likes = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_OK).when()
                 .get("/blog/user/1/post").then().extract().jsonPath().getInt("likesCount[1]");
