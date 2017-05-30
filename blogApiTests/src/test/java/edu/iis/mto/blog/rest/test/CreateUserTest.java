15942e6529a791a69c8bc762565f5735de24b7b7
ciupa.kamil <194928@edu.p.lodz.pl>
2017-05-30 14:19:47 +0200

    3.4 Test unikatowosci pola email

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -2,6 +2,7 @@ package edu.iis.mto.blog.rest.test;
 
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
@@ -9,6 +10,10 @@ import com.jayway.restassured.http.ContentType;
 
 public class CreateUserTest extends FunctionalTests {
 
+	String HEADER_1 = "Content-Type";
+	String HEADER_2 = "application/json;charset=UTF-8";
+	
+	
     @Test
     public void postFormWithMalformedRequestDataReturnsBadRequest() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
@@ -16,4 +21,27 @@ public class CreateUserTest extends FunctionalTests {
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
                 .post("/blog/user");
     }
+    
+    
+    //@Ignore
+    @Test
+    public void createUserRequireUniqueEmail() {
+    	
+    	JSONObject first = new JSONObject().put("email", "kamil@gmail.com");
+    	JSONObject second = new JSONObject().put("email", "kamil@gmail.com");
+    	
+    	RestAssured.given().accept(ContentType.JSON)
+    	.header(HEADER_1, HEADER_2)
+    	.body(first.toString()).expect().log().all()
+    	.statusCode(HttpStatus.SC_CREATED).when().post("/blog/user");
+    	
+    	RestAssured.given().accept(ContentType.JSON)
+    	.header(HEADER_1, HEADER_2)
+    	.body(second.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when()
+    	.post("/blog/user");
+    	
+    	
+    }
+    
+    
 }
