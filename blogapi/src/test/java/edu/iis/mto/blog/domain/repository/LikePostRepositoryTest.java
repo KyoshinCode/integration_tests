775f1635317bac76e55cbd429ee9f2b917338d93
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-06 16:02:38 +0200

    modificateUserTest created.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -4,8 +4,6 @@ import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +13,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by Wojciech Szczepaniak on 23.05.2017.
@@ -31,7 +32,7 @@ public class LikePostRepositoryTest {
 
     @Before
     public void setUp() {
-        user = createMockUser();
+        user = createUser("Jan", "Kozioł", "john@gmail.com");
         post = createMockBlogPostBy(user);
     }
 
@@ -49,13 +50,37 @@ public class LikePostRepositoryTest {
 
         // then
         List<LikePost> likePosts = likePostRepository.findAll();
-        Assert.assertThat(likePosts, Matchers.hasSize(1));
-        Assert.assertThat(likePosts.get(0), Matchers.equalTo(likePost));
+        assertThat(likePosts, hasSize(1));
+        assertThat(likePosts.get(0), equalTo(likePost));
+    }
+
+    @Test
+    public void modificateUserTest() {
+        // given
+        // Prepare repositories.
+        userRepository.save(user);
+        blogPostRepository.save(post);
+        LikePost likePost = createLikePost(post, user);
+        likePostRepository.save(likePost);
+        post.getLikes().add(likePost);
+        blogPostRepository.save(post);
+
+        LikePost post = likePostRepository.findAll().get(0);
+
+        // when
+        User otherUser = createUser("Wojtek", "XYZ", "a@w.pl");
+        userRepository.save(otherUser);
+        post.setUser(otherUser);
+        likePostRepository.save(post);
+
+        // then
+        post = likePostRepository.findAll().get(0);
+        assertThat(post.getUser(), not(equalTo(user)));
+        assertThat(post.getUser(), equalTo(otherUser));
     }
 
     private LikePost createLikePost(BlogPost post, User user) {
         LikePost like = new LikePost();
-        like.setId(1L);
         like.setPost(post);
         like.setUser(user);
 
@@ -66,19 +91,18 @@ public class LikePostRepositoryTest {
         BlogPost post = new BlogPost();
 
         post.setEntry("test");
-        post.setId(1L);
         post.setUser(user);
         post.setLikes(new ArrayList<>());
 
         return post;
     }
 
-    private User createMockUser() {
+    private User createUser(String firstName, String lastName, String eMail) {
         User user = new User();
 
-        user.setFirstName("Jan");
-        user.setLastName("Kowalski");
-        user.setEmail("john@domain.com");
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(eMail);
         user.setAccountStatus(AccountStatus.NEW);
 
         return user;
