e482e91f64316905dde34b2ef9e6ba96f0b4888c
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-06-06 14:09:21 +0200

    Dodano test sprawdzajacy dodawanie like posta

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikeRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikeRepositoryTest.java
@@ -2,7 +2,6 @@ package edu.iis.mto.blog.domain.repository;
 
 import java.util.List;
 
-import org.hamcrest.Matcher;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -16,11 +15,65 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 
 public class LikeRepositoryTest {
+	
+	@Autowired
+    private TestEntityManager entityManager;
+    
+	@Autowired
+    private UserRepository userRepository;
+    
+	@Autowired
+    private LikePostRepository likePostRepository;
+	
+	@Autowired
+    private BlogPostRepository blogPostRepository;
+	
+	private User firstUser;
+	private User secondUser;
+	private BlogPost post;
+	private LikePost like;
+	
+	@Before
+	
+	public void setUp() {
+		firstUser = new User();
+		firstUser.setFirstName("Jan");
+		firstUser.setEmail("jan@domain.com");
+		firstUser.setAccountStatus(AccountStatus.CONFIRMED);
+		firstUser = userRepository.save(firstUser);
+		
+		secondUser = new User();
+		secondUser.setFirstName("Michal");
+		secondUser.setEmail("michal@domain.com");
+		secondUser.setAccountStatus(AccountStatus.CONFIRMED);
+		secondUser = userRepository.save(secondUser);
+		
+		post = new BlogPost();
+		post.setUser(firstUser);
+		post.setEntry("Test");
+		post = blogPostRepository.save(post);
+	}
+	
+	@Test
+	public void shouldFindOnePost() {
 
+		LikePost likePost = new LikePost();
+		likePost.setPost(post);
+		likePost.setUser(secondUser);
+		
+		likePost = likePostRepository.save(likePost);
+		
+		entityManager.refresh(post);
+		
+		assertThat(post.getLikes(), hasSize(1));
+		assertThat(post.getLikes().get(0).getId(), is(equalTo(likePost.getId())));
+	}
+	
 }
