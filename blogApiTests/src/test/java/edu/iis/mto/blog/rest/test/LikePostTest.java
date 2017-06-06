f41842466d6e57cee4289e099d175dd173070ee7
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-06 14:25:52 +0200

    3.4 - naprawa testów w LikePostTest

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -12,19 +12,18 @@ import com.jayway.restassured.http.ContentType;
 
 public class LikePostTest extends FunctionalTests {
 
-	private JSONObject jsonPostObj;
+	private JSONObject jsonObj;
 
     @Before
     public void setUp() {
-        JSONObject jsonPostObj = new JSONObject().put("entry", "post");
+        jsonObj = new JSONObject().put("entry", "post");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonPostObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
                 .post("/blog/user/1/post");
     }
 
 	@Test
     public void authorShouldNotLikeOwnPost() {
-		JSONObject jsonObj = new JSONObject().put("entry", "Post");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
                 .post("/blog/user/1/like/1");
@@ -32,24 +31,24 @@ public class LikePostTest extends FunctionalTests {
 
 	@Test
     public void onlyConfirmedUserCanLikePost() {
-		JSONObject jsonObj = new JSONObject().put("entry", "Post");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
                 .post("/blog/user/2/like/1");
 	}
 
+	@Test
     public void oneUserCannotLikeTwiceSamePost() {
 
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonPostObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
                 .post("/blog/user/3/like/1");
 
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonPostObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
                 .post("/blog/user/3/like/1");
 
         int likesCount = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-                .body(jsonPostObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_OK).when()
                 .get("/blog/user/1/post").then().extract().
                         jsonPath().getInt("likesCount[0]");
 
