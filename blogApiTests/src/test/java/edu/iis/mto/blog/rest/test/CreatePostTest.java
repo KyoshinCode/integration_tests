c22a199a4eb7bb59ec7612cb7d2607d02eb3febe
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-06 11:33:04 +0200

    3.4 - testy dla statusów NEW i CONFIRMED

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -0,0 +1,28 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class CreatePostTest extends FunctionalTests {
+
+    @Test
+    public void userWithStatusConfirmedShouldAddPost() {
+        JSONObject jsonObj = new JSONObject().put("entry", "post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user/1/post");
+    }
+
+    @Test
+    public void userWithStatusNewShouldNotAddPost() {
+        JSONObject jsonObj = new JSONObject().put("entry", "post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/2/post");
+    }
+}
+
