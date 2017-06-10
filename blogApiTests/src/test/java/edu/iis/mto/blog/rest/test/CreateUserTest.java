4c4628051502c3301f19da2eb050abc563e9de0e
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-06-10 10:47:55 +0200

    3.4.1 Dodanie testu - pole email musi być unikatowe

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -9,11 +9,37 @@ import com.jayway.restassured.http.ContentType;
 
 public class CreateUserTest extends FunctionalTests {
 
+	
     @Test
     public void postFormWithMalformedRequestDataReturnsBadRequest() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
                 .post("/blog/user");
+    }
+    
+    @Test
+    public void createUserShouldHaveUniqueEmail() {
+    	
+        JSONObject first = new JSONObject()
+        		.put("email", "jakub@dom.com")
+        		.put("firstName", "Jakub")
+        		.put("lastName", "Kowalski");
+        
+        RestAssured.given().accept(ContentType.JSON)
+    	.header("Content-Type", "application/json;charset=UTF-8")
+    	.body(first.toString()).expect().log().all()
+    	.statusCode(HttpStatus.SC_CREATED).when().post("/blog/user");
+        
+        JSONObject second = new JSONObject()
+        		.put("email", "jakub@dom.com")
+        		.put("firstName", "Jacob")
+        		.put("lastName", "Mild");
+        
+        RestAssured.given().accept(ContentType.JSON)
+        		.header("Content-Type", "application/json;charset=UTF-8")
+                .body(second.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when()
+                .post("/blog/user");
+    	
     }
 }
