2158f0ffa944ba7e5f45f350bd3ea110930527d4
195868 Kwiecien Gaspar <195868@edu.p.lodz.pl>
2017-06-12 02:54:43 +0200

    Dodano test autor nie moze polubic wlasnego posta

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -0,0 +1,20 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.Before;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class LikePostTest extends FunctionalTests {
+	
+	@Test
+	public void postAutorCannotLikeOwnPost() {
+		JSONObject jsonObj = new JSONObject().put("entry", "Post");
+		RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+				.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+				.post("/blog/user/1/like/1");
+	}
+}
