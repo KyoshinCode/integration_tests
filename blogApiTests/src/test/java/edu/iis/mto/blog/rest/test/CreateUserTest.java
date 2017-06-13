010e630ff65782d8d6e7ffb8db67498906916779
Iryna Shkliarenko <iryna.shkliarenko@tomtom.com>
2017-06-13 11:00:04 +0200

    3.4 CreateUserTest: refactorization

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -9,24 +9,23 @@ import com.jayway.restassured.http.ContentType;
 
 public class CreateUserTest extends FunctionalTests {
 
-//    @Test
-//    public void postFormWithMalformedRequestDataReturnsBadRequest() {
-//        JSONObject jsonObj = new JSONObject().put("email", "trace@domain.com").put("firstName", "Trace");
-//        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-//                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
-//                .post("/blog/user");
-//    }
+    @Test
+    public void postFormWithMalformedRequestDataReturnsBadRequest() {
+        JSONObject jsonObj = new JSONObject().put("email", "trace@domain.com").put("firstName", "Trace");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user");
+    }
     
     @Test
     public void verifyIfUserHaveUniqueEmail() throws Exception {
         JSONObject jsonObj = new JSONObject().put("accountStatus", "NEW").put("email", "kapibarson@awesome.com").put("firstName", "John").put("lastName", "Kapibara");
-//        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
-//                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
-//                .post("/blog/user");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user");
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
         		.body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when()
         		.post("/blog/user");
-        
     }
     
 }
