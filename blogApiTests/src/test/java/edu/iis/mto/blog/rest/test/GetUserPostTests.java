3fb1db8d4f8fc2a0eb65f44648ce090b4399eb6e
KhaledWielki <adam.rezner95@gmail.com>
2017-06-14 23:12:17 +0200

    3.4 user status = removed => cant search it's posts

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/GetUserPostTests.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.blog.rest.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class GetUserPostTests extends FunctionalTests {
+
+	@Test
+    public void getREMOVEDUserPostsShouldFail() {
+		JSONObject jsonObj = new JSONObject().put("entry", "Post");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/4/post");
+	}
+
+}
