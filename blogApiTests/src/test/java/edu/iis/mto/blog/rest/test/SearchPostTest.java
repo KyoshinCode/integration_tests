483a064e652de0911122ca918f79447aedfc93c1
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-16 22:52:20 +0200

    searchUserPostsReturnPostsWithCorrectLikesNumber test created.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchPostTest.java
@@ -2,8 +2,12 @@ package edu.iis.mto.blog.rest.test;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.path.json.JsonPath;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Wojciech Szczepaniak on 16.06.2017.
@@ -15,5 +19,26 @@ public class SearchPostTest extends FunctionalTests {
         RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
                 .expect().log().all().statusCode(HttpStatus.SC_NOT_FOUND)
                 .when().get("/blog/user/3/post");
+    }
+
+    @Test
+    public void searchUserPostsReturnPostsWithCorrectLikesNumber() {
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK)
+                .when().post("/blog/user/4/like/3");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK)
+                .when().post("/blog/user/5/like/3");
+
+        JsonPath request = RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .expect().log().all().statusCode(HttpStatus.SC_OK)
+                .when().get("/blog/user/1/post")
+                .then().extract().jsonPath();
+
+        int postNumber = request.getInt("$.size()");
+        assertThat(postNumber, equalTo(2));
+
+        int likesNumber = request.getInt("likesCount[1]");
+        assertThat(likesNumber, equalTo(2));
     }
 }
