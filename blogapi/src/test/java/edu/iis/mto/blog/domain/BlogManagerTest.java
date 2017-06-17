467a73793cc2a72d9fee30813ca5954811d65c99
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-06-17 12:15:12 +0200

    BUGFIX mock userRepository return CONFIRMED user in likePostByUserConfirmed test.

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -89,11 +89,11 @@ public class BlogManagerTest {
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
         verify(userRepository, times(2)).save(userParam.capture());
         List<User> users = userParam.getAllValues();
+        users.get(0).setAccountStatus(AccountStatus.CONFIRMED);
         users.get(1).setAccountStatus(AccountStatus.CONFIRMED);
-        verify(userRepository, times(2)).save(userParam.capture());
-        users = userParam.getAllValues();
 
         // Dodaj post
+        when(userRepository.findOne(anyLong())).thenReturn(users.get(0));
         blogService.createPost(users.get(0).getId(), new PostRequest());
         ArgumentCaptor<BlogPost> postParam = ArgumentCaptor.forClass(BlogPost.class);
         verify(blogPostRepository).save(postParam.capture());
