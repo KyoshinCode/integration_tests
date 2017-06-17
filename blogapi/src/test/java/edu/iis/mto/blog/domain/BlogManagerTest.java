68767ed7b5107b19ce181d2851adb6489938bf49
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-06-17 21:03:17 +0200

    setup - BlogManagerTest

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,7 +1,13 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -18,6 +24,12 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
+import static edu.iis.mto.blog.domain.model.AccountStatus.NEW;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -31,13 +43,47 @@ public class BlogManagerTest {
     @Autowired
     BlogService blogService;
 
+    @MockBean
+    BlogPostRepository blogRepository;
+
+    @MockBean
+    LikePostRepository likeRepository;
+
+    private User firstUser;
+    private User secondUser;
+    private BlogPost blogPost;
+
+    @Before
+    public void setUp(){
+
+        firstUser = new User();
+        firstUser.setFirstName("Adam");
+        firstUser.setLastName("Nowak");
+        firstUser.setEmail("adamnowak@email.pl");
+        firstUser.setAccountStatus(NEW);
+        firstUser.setId(1L);
+
+        secondUser = new User();
+        secondUser.setFirstName("Jakub");
+        secondUser.setLastName("Adamiak");
+        secondUser.setEmail("jakubadamiak@email.pl");
+        secondUser.setAccountStatus(AccountStatus.CONFIRMED);
+        secondUser.setId(2L);
+
+        blogPost = new BlogPost();
+        blogPost.setId(1L);
+        blogPost.setUser(secondUser);
+        blogPost.setEntry("Post");
+    }
+
+
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
-        Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+        assertThat(user.getAccountStatus(), Matchers.equalTo(NEW));
     }
 
 }
