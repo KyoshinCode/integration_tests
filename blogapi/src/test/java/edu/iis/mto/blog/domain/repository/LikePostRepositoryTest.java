cc6fd90a6a3d673a82960c7584081b50f1e5ece9
Maciek <miedzianowski.maciek@gmail.com>
2017-06-18 20:07:42 +0200

    Implementacja testu shouldNotFindAnyLikedPost + refactor setup

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -1,13 +1,23 @@
 package edu.iis.mto.blog.domain.repository;
 
 import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -33,9 +43,13 @@ public class LikePostRepositoryTest {
     }
 
     private void setupAndSaveSamplePost() {
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(user);
+        blogPost.setEntry("entry");
+
         LikePost likePost = new LikePost();
         likePost.setUser(user);
-        likePost.setPost(blogPostRepository.findAll().get(0));
+        likePost.setPost(blogPost);
     }
 
     private void setupAndSaveUserForTests() {
@@ -49,5 +63,15 @@ public class LikePostRepositoryTest {
         user.setEmail("jankowalski@mail.com");
 
         userRepository.save(user);
+    }
+
+    @Test
+    public void shouldNotFindAnyLikedPost() throws Exception {
+
+        //when:
+        List<LikePost> result = likePostRepository.findAll();
+
+        //then:
+        assertThat(result, hasSize(0));
     }
 }
