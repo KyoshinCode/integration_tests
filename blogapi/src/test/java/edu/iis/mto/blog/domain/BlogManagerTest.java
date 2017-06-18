af479c66db661b1ed7ef3ded87a2186651f09a65
Maciek <miedzianowski.maciek@gmail.com>
2017-06-18 21:37:25 +0200

    Implementacja testu shouldConfirmedUserCanLikePosts

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,8 +1,10 @@
 package edu.iis.mto.blog.domain;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import edu.iis.mto.blog.domain.errors.DomainError;
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,6 +23,11 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.DataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
@@ -30,6 +37,9 @@ public class BlogManagerTest {
 
     @MockBean
     BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
 
     @Autowired
     DataMapper dataMapper;
@@ -43,7 +53,7 @@ public class BlogManagerTest {
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
-        Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+        assertThat(user.getAccountStatus(), equalTo(AccountStatus.NEW));
     }
 
     @Test(expected = DomainError.class)
@@ -68,4 +78,39 @@ public class BlogManagerTest {
         //when:
         blogService.addLikeToPost(newUser.getId(), newBlogPost.getId());
     }
+
+    @Test
+    public void shouldConfirmedUserCanLikePost() throws Exception {
+        //given:
+        User confirmedUser = new User();
+        confirmedUser.setFirstName("Jan");
+        confirmedUser.setLastName("Kowalski");
+        confirmedUser.setEmail("jankowalski@mail.com");
+        confirmedUser.setAccountStatus(AccountStatus.CONFIRMED);
+        confirmedUser.setId(1L);
+
+        User anyUser = new User();
+        anyUser.setFirstName("Jan");
+        anyUser.setLastName("Kowalski");
+        anyUser.setEmail("jankowalski@mail.com");
+        anyUser.setAccountStatus(AccountStatus.CONFIRMED);
+        anyUser.setId(2L);
+
+        BlogPost newBlogPost = new BlogPost();
+        newBlogPost.setUser(anyUser);
+        newBlogPost.setEntry("entry");
+        newBlogPost.setId(1L);
+
+        Mockito.when(userRepository.findOne(confirmedUser.getId())).thenReturn(confirmedUser);
+        Mockito.when(userRepository.findOne(anyUser.getId())).thenReturn(anyUser);
+        Mockito.when(blogPostRepository.findOne(newBlogPost.getId())).thenReturn(newBlogPost);
+        Mockito.when(likePostRepository.findByUserAndPost(confirmedUser, newBlogPost)).thenReturn(Optional.empty());
+
+        //when:
+        Boolean result = blogService.addLikeToPost(confirmedUser.getId(), newBlogPost.getId());
+
+        //then
+        assertThat(result, equalTo(true));
+    }
+
 }
