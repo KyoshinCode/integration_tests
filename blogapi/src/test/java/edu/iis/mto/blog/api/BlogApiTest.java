5e47e94091cbdfe349e22ffab3d1e6b41a5ed08c
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-13 22:09:40 +0200

    [3.3.0] Added test for HTTP Status 404 + 409 in BlogApi test

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -4,15 +4,19 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -52,6 +56,30 @@ public class BlogApiTest {
 
     private String writeJson(Object obj) throws JsonProcessingException {
         return new ObjectMapper().writer().writeValueAsString(obj);
+    }
+    
+    @Test 
+    public void throwingDataIntegrityViolationExceptionShouldGenerateResponseHTTP409Status() throws Exception {
+    	
+    	UserRequest user = new UserRequest();
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+    	
+    	Mockito.when(blogService.createUser(user)).thenThrow(DataIntegrityViolationException.class);
+    	String content = writeJson(user);
+    	
+    	mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8)
+    			.content(content)).andExpect(status().isConflict());
+    }
+    
+    @Test
+    public void downloadingDataOfNotExistingDataShouldGenerateResponseHTTP404Status() throws Exception {
+    	
+    	Long newUserId = 2L;
+    	
+    	Mockito.when(finder.getUserData(newUserId)).thenThrow(new EntityNotFoundException(String.format("user with id %d not exists", newUserId)));
+    	mvc.perform(get("/blog/user/2").accept(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isNotFound());
     }
 
 }
