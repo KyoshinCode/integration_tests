b51cd26a450b74f6d9ce7a79038590321ba09756
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-13 22:45:26 +0200

    [3.4.1] Added test for unique email

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -16,4 +16,29 @@ public class CreateUserTest extends FunctionalTests {
                 .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
                 .post("/blog/user");
     }
+    
+    @Test
+    public void createdUserShouldHaveUniqueEmail() {
+    	
+        JSONObject first = new JSONObject()
+        		.put("email", "kyoshin@code.com")
+        		.put("firstName", "Kyoshin")
+        		.put("lastName", "Code");
+        
+        RestAssured.given().accept(ContentType.JSON)
+    	.header("Content-Type", "application/json;charset=UTF-8")
+    	.body(first.toString()).expect().log().all()
+    	.statusCode(HttpStatus.SC_CREATED).when().post("/blog/user");
+        
+        JSONObject second = new JSONObject()
+        		.put("email", "kyoshin@code.com")
+        		.put("firstName", "Code")
+        		.put("lastName", "Kyo");
+        
+        RestAssured.given().accept(ContentType.JSON)
+        		.header("Content-Type", "application/json;charset=UTF-8")
+                .body(second.toString()).expect().log().all().statusCode(HttpStatus.SC_CONFLICT).when()
+                .post("/blog/user");
+    	
+    }
 }
