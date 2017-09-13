b20993044cd378ffe3efb3762a739d78ee8589c2
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-13 21:29:53 +0200

    [3.1.2] Added tests for positive and negative results

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.fasterxml.jackson.databind.jsontype.impl.AsExistingPropertyTypeSerializer;
+
 import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.User;
 
@@ -33,6 +35,7 @@ public class UserRepositoryTest {
         user = new User();
         repository.deleteAll();
         user.setFirstName("Rafal");
+        user.setLastName("Franiewski");
         user.setEmail("194944@edu.p.lodz.pl");
         user.setAccountStatus(AccountStatus.NEW);
     }
@@ -62,4 +65,29 @@ public class UserRepositoryTest {
         Assert.assertThat(persistedUser.getId(), Matchers.notNullValue());
     }
 
+    @Test 
+    public void shouldFindUserWithProperEmail() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("", "", "194944@edu.p.lodz.pl");
+
+        Assert.assertThat(users, Matchers.hasSize(1));
+        Assert.assertThat(users.get(0).getEmail(), Matchers.equalTo("194944@edu.p.lodz.pl"));
+    }
+
+    @Test
+    public void shouldNotFindUserWithWrongInformation() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Kyoshin", "Code", "K@C.com");
+
+        Assert.assertThat(users, Matchers.hasSize(0));
+    }
+
+    @Test
+    public void shouldFindUserWithGivenName() {
+        repository.save(user);
+        List<User> users = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Rafal", "", "");
+   
+        Assert.assertThat(users, Matchers.hasSize(1));
+        Assert.assertThat(users.get(0).getFirstName(), Matchers.equalTo("Rafal"));
+    }
 }
