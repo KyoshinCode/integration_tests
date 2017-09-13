17681da988fded4129b8eca393ab33fdc7b4f65a
Rafal Franiewski <194944@edu.p.lodz.pl>
2017-09-13 22:50:27 +0200

    [3.4.3] Added test for adding like

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikeTest.java
@@ -0,0 +1,31 @@
+package edu.iis.mto.blog.rest.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class AddLikeTest {
+	
+	@Test
+    public void addingLikeShouldCheckCONFIRMEDStatusOfAccount() {
+		
+		JSONObject first = new JSONObject().put("entry", "First test for like");
+		
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(first.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/1/like/1");
+	}
+	
+	@Test
+    public void authorOfPostCannotLikeOwnPost() {
+		
+		JSONObject second = new JSONObject().put("entry", "Second test for like");
+		
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(second.toString()).expect().log().all().statusCode(HttpStatus.SC_BAD_REQUEST).when()
+                .post("/blog/user/2/like/1");
+	}
+
+}
